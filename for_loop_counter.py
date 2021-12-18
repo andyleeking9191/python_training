3cd47c724d66b12657e520885b79601f27956711
Andrew King <andyleeking9191@yahoo.com>
2021-12-18 14:17:24 -0500

    for loop list counter

---
+++ for_loop_counter.py
@@ -0,0 +1,9 @@
+# Challenge: Create a for loop that iterates over the list of numbers and outputs the total numbers in that list.
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+counter = 0
+
+for num in my_list:
+    counter += 1
+
+print(f'There are {counter} numbers in the list')
